55485b58882622b4ea7d2b17d65d9236de753459
David <softw.dev.dave@gmail.com>
2021-02-28 08:32:02 +0100

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+from django.urls import reverse
+
+'''
+Category
+=========
+title, slug
+
+Tag
+=========
+title, slug
+
+Post
+=========
+title, slug, author, content, created_at, photo, views, category, tags,
+
+'''
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={"slug": self.slug})
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['title']
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, verbose_name='Url', unique=True)
+
+    def get_absolute_url(self):
+        return reverse('tag', kwargs={"slug": self.slug})
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+        ordering = ['title']
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
+    author = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='posted')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    views = models.IntegerField(default=0, verbose_name='Кол-во просмотров')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={"slug": self.slug})
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+        ordering = ['-created_at']
